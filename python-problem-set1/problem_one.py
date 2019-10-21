93e595c2aaf9da5a6a6b57998632749e673550dd
Gajendra mani <gtripathi@philips.com>
2019-10-21 11:37:44 +0530

    git add

--- python-problem-set1/problem_one.py
+++ python-problem-set1/problem_one.py
@@ -1,10 +1,28 @@
+#**********************************************************************
 # Find out the sum of occured digit in given strings
+#**********************************************************************
+
 import os
 import re
-import file
-import strings
+import string
 
 
 def string_digit_sum (inputString):
-    splitBySpace = inputString.split('')
-    print (splitBySpace)
+    splitBySpace = list(inputString)
+    lengthOfList = len(splitBySpace)
+    #print (lengthOfList)
+    numricList = []
+    for x in (splitBySpace):
+        if x.isdigit():
+            y=int(x)
+            numricList.append(y)
+    print ("Sum of List is", sum(numricList))
+
+            
+
+    
+
+
+if (__name__ == "__main__"):
+    valueString = '1Abc32def34jklm980'
+    string_digit_sum(valueString)
