090ff5c77824beeacc97a1d40bba7a69fa30e971
Gajendra mani <gtripathi@philips.com>
2019-10-21 10:54:19 +0530

    Added Problem Set one

---
+++ python-problem-set1/problem_one.py
@@ -0,0 +1,10 @@
+# Find out the sum of occured digit in given strings
+import os
+import re
+import file
+import strings
+
+
+def string_digit_sum (inputString):
+    splitBySpace = inputString.split('')
+    print (splitBySpace)
