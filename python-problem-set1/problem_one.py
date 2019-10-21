983f651c7e9d6159aec07ee751469100364296f3
Gajendra mani <gtripathi@philips.com>
2019-10-21 14:09:01 +0530

    push

--- python-problem-set1/problem_one.py
+++ python-problem-set1/problem_one.py
@@ -1,10 +1,12 @@
-#**********************************************************************
+#************************************************************************************************************
 # Find out the sum of occured digit in given strings
-#**********************************************************************
+# 1.1 ==> if two or more digit is consecutive then treat as a one digit accoring to that make addition logic
+#************************************************************************************************************
 
 import os
 import re
 import string
+import re
 
 
 def string_digit_sum (inputString):
@@ -18,6 +20,19 @@ def string_digit_sum (inputString):
             numricList.append(y)
     print ("Sum of List is", sum(numricList))
 
+
+def consecutive_add (inputString):
+    splitBysequence = re.compile("[a-z]|[A-Z]").split(inputString)
+    #splitBysequence = re.split(r"\c", inputString)
+    #splitByEscape = inputString.split("[a-z]|[A-Z]")
+    numricList = []
+    for x in (splitBysequence):
+        if x.isdigit():
+            y=int(x)
+            numricList.append(y)
+    print ("Sum of List is By Using Second Condition", sum(numricList))
+    
+
             
 
     
@@ -26,3 +41,4 @@ def string_digit_sum (inputString):
 if (__name__ == "__main__"):
     valueString = '1Abc32def34jklm980'
     string_digit_sum(valueString)
+    consecutive_add(valueString)
