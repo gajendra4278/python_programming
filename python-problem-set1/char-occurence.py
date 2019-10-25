c82cdfffb6193be12682c4d481ecd98dad7a8988
Gajendra mani <gtripathi@philips.com>
2019-10-25 16:33:43 +0530

    Removed Print Statement

--- python-problem-set1/char-occurence.py
+++ python-problem-set1/char-occurence.py
@@ -7,17 +7,14 @@ import sys
 inputString = "This is my name but i am not blah blah blah.."
 sampleDict = {}
 for i in (inputString):
-    print (i)
     keys = sampleDict.keys()
     values= sampleDict.values()
-    print (keys)
-    print (values)
     if (i in keys):
         sampleDict[i] += 1  # sampleDict[i] = sampleDict[i] +1
     else:
-        print ("Bye")
         sampleDict[i] = 1
-#print (sampleDict)
+
+print (sampleDict)
 #-------------------Count of Input Char From the Strings -----------------------
 #count = 0 
 #charValue = 'b' 
